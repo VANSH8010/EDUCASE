b9ca35661bdc28dc1e6f32c0f63f00c5388727b4
VANSH8010 <144270044+VANSH8010@users.noreply.github.com>
2024-08-24 22:50:35 +0530

    1.3

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Create a pool of a MySQL connection
 const connection = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Vansh@2003',
-    database: process.env.DB_NAME || 'schooldb',
-    connectionLimit :process.env.DB_LIMIT || 10,
+    host: process.env.DB_HOST || 'sql12.freemysqlhosting.net',
+    user: process.env.DB_USER || 'sql12727646',
+    password: process.env.DB_PASSWORD || '8uLzr5dDha',
+    database: process.env.DB_NAME || 'sql12727646',
+    connectionLimit :process.env.DB_LIMIT || 5,
     port: process.env.DB_PORT || 3306,
 
 }); 
